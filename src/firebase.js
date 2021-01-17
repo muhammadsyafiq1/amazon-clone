25000e6aa44419b707a2cd5a7e742633eceff67c
unknown <syafiqmuhammad655@gmail.com>
2021-01-17 12:53:02 -0800

    insatll react install firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBG-o3JsFWMe8myr5B4NBU9MMg-w4cOwpM",
+    authDomain: "fir-ad412.firebaseapp.com",
+    projectId: "fir-ad412",
+    storageBucket: "fir-ad412.appspot.com",
+    messagingSenderId: "334818114455",
+    appId: "1:334818114455:web:0cf2c0faa62980aad362d2",
+    measurementId: "G-WJFSC1X1FS"
+  };
