975163463d03f57554d92f753e5db3c86c6b33da
unknown <syafiqmuhammad655@gmail.com>
2021-01-25 20:35:45 -0800

    fitur registrasi dengan firebase dan penggunaan useHistory

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import Home from "./pages/home/Home"
 import Header from "./components/header/Header"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Checkout from './pages/checkout/Checkout'
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <Router>
        <div className="app">
-          <Header/>
           <Switch>
 
+            <Route path="/login">
+              <Login/>
+            </Route>
+              
               <Route path="/checkout">
+                <Header/>
                 <Checkout/>
               </Route>
 
               <Route path="/">
+                <Header/>
                 <Home/>
-              </Route>
-              
+              </Route>   
+
           </Switch>
         </div> 
     </Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBG-o3JsFWMe8myr5B4NBU9MMg-w4cOwpM",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     appId: "1:334818114455:web:0cf2c0faa62980aad362d2",
     measurementId: "G-WJFSC1X1FS"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
+
+  export {db,auth};
